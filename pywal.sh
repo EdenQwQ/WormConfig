6904f380d61fc5da6f488d87c465f5bac2553e9a
EdenQwQ <lsahlm1eden@gmail.com>
2021-11-15 17:26:11 +0800

    changed polybar theme, added polybar pywal support

---
+++ pywal.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+WMNAME=`wmctrl -m | sed -n 's/Name: //p'`
+
+wal -i $(sed -n 's/file=//p' ~/.config/nitrogen/bg-saved.cfg) -q -t
+
+if [[ "$WMNAME" == "worm" ]]; then
+
+    PFILE="$HOME/.config/polybar/hack/colors.ini"
+
+    . "$HOME/.cache/wal/colors.sh"
+
+    BG=`printf "%s\n" "$background"`
+    FG=`printf "%s\n" "$foreground"`
+    AC=`printf "%s\n" "$color1"`
+    C1=`printf "%s\n" "$color1"`
+    C2=`printf "%s\n" "$color2"`
+    C3=`printf "%s\n" "$color6"`
+
+    sed -i -e "s/background = #.*/background = $BG/g" $PFILE
+    sed -i -e "s/foreground = #.*/foreground = $FG/g" $PFILE
+    sed -i -e "s/primary = #.*/primary = $AC/g" $PFILE
+    sed -i -e "s/color1 = #.*/color1 = $C1/g" $PFILE
+    sed -i -e "s/color2 = #.*/color2 = $C2/g" $PFILE
+    sed -i -e "s/color3 = #.*/color3 = $C3/g" $PFILE
+
+    polybar-msg cmd restart
+
+elif [[ "$WMNAME" == "LG3D" ]]; then
+    
+    qtile cmd-obj -o cmd -f restart
+
+fi
+
